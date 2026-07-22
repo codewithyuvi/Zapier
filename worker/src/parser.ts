8a5f7ec93abe8257fc6f5c63ec50d9013b089238
codewithyuvi <yuvrajbansal30dec@gmail.com>
2026-07-22 23:21:13 +0530

    feat: add parseDynamicData function for dynamic data parsing in templates

---
+++ worker/src/parser.ts
@@ -0,0 +1,6 @@
+export function parseDynamicData(template: string, payload: any): string {
+    if(!template) return "";
+    return template.replace(/\{payload\.([^}]+)\}/g, (_, key) => {
+        return payload[key] !== undefined ? String(payload[key]) : "";
+    })
+}
