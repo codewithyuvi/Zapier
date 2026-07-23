ecb2e3afb4a83988e30c66260c725d0355c6c38b
codewithyuvi <yuvrajbansal30dec@gmail.com>
2026-07-24 00:35:20 +0530

    feat: add Redis and rate limiting support to webhook handler

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,12 +1,40 @@
 import { db, triggerOutbox } from '@zapier/database';
 import express from 'express';
 import type { Request, Response } from 'express';
+import {Redis} from 'ioredis';
+import rateLimit from 'express-rate-limit';
+import {RedisStore} from 'rate-limit-redis';
+
 const app = express();
 const PORT = 3002;
 
 app.use(express.json());
 
-app.post('/hooks/catch/:userId/:zapId', async (req: Request<{userId: string; zapId: string}>, res: Response) => {
+const redisClient = new Redis({
+  host: 'localhost',
+  port: 6379,
+});
+
+redisClient.on('error', (err) => {
+  console.error('Redis connection error:', err);
+});
+
+const webhookRateLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000, 
+  max: 10, 
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: { error: "Too many webhook requests. Please try again later." },
+  store: new RedisStore({
+    // @ts-expect-error - Known typing issue with rate-limit-redis and ioredis
+    sendCommand: (...args: string[]) => redisClient.call(...args),
+  }),
+  keyGenerator: (req) => {
+    return `ratelimit_user_${req.params.userId}`;
+  }
+});
+
+app.post('/hooks/catch/:userId/:zapId', webhookRateLimiter ,async (req: Request<{userId: string; zapId: string}>, res: Response) => {
   const userId = req.params.userId;
   const zapId = req.params.zapId;
 
